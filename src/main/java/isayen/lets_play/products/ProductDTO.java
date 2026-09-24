77b6aed3ecb3da6715fc05a31df99730f7da75bd
ismailsayen <ismailsvn02@gmail.com>
2026-09-24 11:56:18 +0000

    Added SSL configuration, updated configuration files, added a certificate generation script, and improved user and product management. Also document the rproject by adding a detailled readme.md

--- src/main/java/isayen/lets_play/products/ProductDTO.java
+++ src/main/java/isayen/lets_play/products/ProductDTO.java
@@ -1,6 +1,5 @@
 package isayen.lets_play.products;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
